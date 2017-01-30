e43b8f495f1d25253ea6c8aca21d4d4d39b92aa7
Parimala4771 <parimala.mbnr@gmail.com>
2017-01-29 18:40:09 -0600

    Add files via upload

---
+++ Customizedxpath.java
@@ -0,0 +1,20 @@
+package basics;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Customizedxpath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.gecko.driver","E:\\Softwares\\geckodriver-v0.13.0-win64\\geckodriver.exe");
+WebDriver driver= new FirefoxDriver();
+driver.get("http://www.facebook.com");
+driver.findElement(By.xpath("//input[@id='email']")).sendKeys("parimala.nijalapuram@gmail.com");
+driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("dabang");
+driver.findElement(By.xpath("//input[@value='Log In']")).click();
+	}
+
+}
